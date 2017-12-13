ffc666a95a1dc3a2277d9cee8559a44bc30db97a
mohamed mhiri <med.mhiri.5@gmail.com>
2017-12-13 12:31:35 +0100

    basic appclient and adapter implemented

--- src/main/java/finance/AppClient.java
+++ src/main/java/finance/AppClient.java
@@ -7,7 +7,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import quickfix.*;
+//import quickfix.MessageStoreFactory;
+import quickfix.*;/*
+import quickfix.
+import quickfix.
+import quickfix.
+import quickfix.
+import quickfix.
+import quickfix.*/
 
 @EnableQuickFixJClient
 @SpringBootApplication
